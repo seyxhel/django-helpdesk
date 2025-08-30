9715ddad6892157a8529e303bc3b59ea071ef3cf
seyxhel <sealtieljosephbpelagio@iskolarngbayan.pup.edu.ph>
2025-08-30 17:12:59 +0800

    feat: Update SLA staff page to display tickets assigned to the user ordered by priority

--- helpdesk/views/sla_staff.py
+++ helpdesk/views/sla_staff.py
@@ -1,6 +1,11 @@
 from helpdesk.decorators import helpdesk_staff_member_required
 from django.shortcuts import render
+from helpdesk.models import Ticket
 
+
+@helpdesk_staff_member_required
 def sla_staff(request):
-    """Staff SLA overview page."""
-    return render(request, 'helpdesk/sla_staff.html', {})
+    """Staff SLA overview page: list tickets assigned to the current user ordered by priority."""
+    user = request.user
+    tickets = Ticket.objects.filter(assigned_to=user).order_by('priority', 'created')[:100]
+    return render(request, 'helpdesk/sla_staff.html', {'tickets': tickets})
